057f02f0dca1508b5a819044ee301732e004808f
JinDannyDavid <71433164@continental.edu.pe>
2024-09-02 18:52:16 -0500

    Proyecto suma en POO

---
+++ OperacionesAritmeticas.py
@@ -0,0 +1,12 @@
+class OperacionesAritmeticas():
+    def IngreNumer(self):
+        numero01 = float(input("INGRESE PRIMER NUMERO: "))
+        numero02 = float(input("INGRESE SEGUNDO NUMERO: "))
+        return numero01, numero02
+
+
+    def suma(self, numero1, numero2):
+        return numero1 + numero2
+
+
+
